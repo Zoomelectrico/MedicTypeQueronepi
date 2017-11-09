102308ce678e70998abc36b767a1644beb596688
Germano Rojas <germano.rojas@correo.unimet.edu.ve>
2017-11-09 14:52:21 -0400

    Actualizar Controladores

---
+++ api/controllers/HistoriaController.js
@@ -0,0 +1,11 @@
+/**
+ * HistoriaController
+ *
+ * @description :: Server-side logic for managing Historias
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
