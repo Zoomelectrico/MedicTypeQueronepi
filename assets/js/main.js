0a05342e41ee0e7d49f0fce10c4d7fcec6158a56
Jose Roberto Quevedo <jose.quevedo2011@gmail.com>
2017-11-09 16:36:34 -0400

    Avances en la UI
    Vistas a casi terminar homepage, logins, registros.

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+  $('.datepicker').pickadate({
+    selectMonths: true,
+    selectYears: 1000, 
+    today: 'Today',
+    clear: 'Clear',
+    close: 'Ok',
+    closeOnSelect: false,
+    format: 'yyyy-mm-dd'
+  });
