20ea4b61cb87f4b3223f60d9633352505502c595
Jose Roberto Quevedo <jose.quevedo2011@gmail.com>
2017-11-10 10:34:02 -0400

    UI añadiendo rutas

--- api/controllers/MedicoController.js
+++ api/controllers/MedicoController.js
@@ -6,30 +6,25 @@
  */
 
 module.exports = {
-	Login: function(req, res) {
- 		res.view('medico-login');
-  	},
- 	Registrar: function(req, res) {
-	    var params = req.body;
-	    Medico.create(params, function(err, createdData) {
-	      if (err) { return res.badRequest({ error: err }) }
-	      res.view('medico-panel', { medico: createdData });
-	    });
-  	},
-  	Home: function(req, res) {
-	    Medico.findOne({ Cedula: req.body.cedula }).exec(function(err, doc) {
-	      if (err) { return res.badRequest({ error: err }) }
-	      Consulta.query(
-	        'SELECT Paciente.Apellido, Paciente.Nombre' +
-	        'From historia INNER JOIN medico on historia.Medico = medico.id' + //+ req.body.id +
-	        'INNER JOIN paciente on paciente.id = historia.Paciente Where medico.id =' + req.body.id + ' AND historia.fecha =' + new Date().getFullYear() + '-' + new Date().getMonth() + '-' + new Date().getDate(),
-	        function(err, rawResult) {
-	          if (err) { return res.serverError(err); }
-	          var pacientes = rawResult.json();
-	          res.view('medico-panel', { doctor: doc, paciente: paciente });
-	        });
-	    })
-	  }
-	  
+  Registrar: function(req, res) {
+    var params = req.body;
+    Medico.create(params, function(err, createdData) {
+      if (err) { return res.badRequest({ error: err }) }
+      res.view('medico-panel', { medico: createdData });
+    });
+  },
+  Login: function(req, res) {
+    Medico.findOne({ Cedula: req.body.cedula }).exec(function(err, doc) {
+      if (err) { return res.badRequest({ error: err }) }
+      Consulta.query(
+        'SELECT Paciente.Apellido, Paciente.Nombre' +
+        'From historia INNER JOIN medico on historia.Medico = medico.id' + ' ' +
+        'INNER JOIN paciente on paciente.id = historia.Paciente Where medico.id =' + doc.body.id + ' AND historia.fecha =' + new Date().getFullYear() + '-' + new Date().getMonth() + '-' + new Date().getDate(),
+        function(err, rawResult) {
+          if (err) { return res.serverError(err); }
+          var pacientes = rawResult.json();
+          res.view('medico-panel', { doctor: doc, pacientes: pacientes });
+        })
+    });
+  }
 };
-
