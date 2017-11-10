43283ea421ef9a6972987e8942c2ebce5ce6a391
Germano Rojas <germano.rojas@correo.unimet.edu.ve>
2017-11-10 10:35:54 -0400

    ,

--- config/routes.js
+++ config/routes.js
@@ -5,7 +5,7 @@ module.exports.routes = {
   },
   'GET /Medico/Login': {view: 'medico-login'},
   'GET /Medico/Registro': {view: 'medico-registro'},
-  'POST /Medico/Home':'MedicoController.Login'
+  'POST /Medico/Home':'MedicoController.Login',
 
   'GET /Paciente/Registro': 'PacienteController.Crear',
   'GET /Paciente/login': {view: 'paciente-login'},
