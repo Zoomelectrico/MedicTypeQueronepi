99f13b4e4f668cf48c1874f1d3ce9373d6572e0f
Germano Rojas <germano.rojas@correo.unimet.edu.ve>
2017-11-10 23:26:00 -0400

    Ya funcionan las consultas del medico

--- api/controllers/MedicoController.js
+++ api/controllers/MedicoController.js
@@ -16,8 +16,12 @@ module.exports = {
   Login: function(req, res) {
     Medico.findOne({ Cedula: req.body.cedula }).exec(function(err, doc) {
       if (err) { return res.badRequest({ error: err }) }
+        var hoy = new Date();
+        var dateAnio =  hoy.getUTCFullYear();
+        var dateMes = hoy.getUTCMonth();
+        var dateDia = hoy.getUTCDate();
       Consulta.query(
-        'SELECT paciente.Apellido, paciente.Nombre FROM consulta INNER JOIN medico on consulta.Medico = medico.id INNER JOIN paciente ON paciente.id = consulta.Paciente WHERE medico.id =' + doc.id +'AND consulta.Fecha = ' + '\''+Date.now().getFullYear()+'-'+Date.now().getMonth()+'-'+Date.now().getDay()+'\'',
+        'SELECT paciente.Apellido, paciente.Nombre FROM consulta INNER JOIN medico on consulta.Medico = medico.id INNER JOIN paciente ON paciente.id = consulta.Paciente WHERE medico.id = '+ doc.id +' AND consulta.Fecha = ' + '\'' + dateAnio + '-' + dateMes + '-' + dateDia + '\'',// + '\'' + dateAnio + '-' + dateMes + '-' + dateDia + '\'' +\'2017-07-07\',
         function(err, rawResult) {
           if (err) { return res.serverError(err); }
           res.view('medico-panel', { medico: doc, pacientes: rawResult});
