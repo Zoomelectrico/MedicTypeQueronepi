9c9acff87fddecb03ebe4d8d8e0ee755830bfff4
Luciano Pinedo <lucianopinedo@gmail.com>
2017-11-15 09:15:55 -0400

    Controlador patologia
    No pueo probarlo porque el server de heroku esta dormido

--- api/controllers/PatologiaController.js
+++ api/controllers/PatologiaController.js
@@ -6,5 +6,25 @@
  */
 
 module.exports = {
+
+    intervaloPatologiasDiagnosticadas: function (req, res) {
+        console.log(req.params.fecha1); 
+        console.log(req.params.fecha2);
+        Consulta.query(
+        'select Consulta.fecha, patologia.nombre, COUNT(patologia.nombre)  from Consulta' +
+       ' INNER JOIN patologias_informe on Consulta.id = patologias_informe.informe ' +
+       ' INNER JOIN patologia on patologias_informe.patologia = patologia.id ' +
+       "WHERE FECHA BETWEEN '" + req.params.fecha1 + "' AND '" + req.params.fecha2 + "'" +
+        "GROUP BY patologia.nombre;" ,
+        function (err, rawResult) {
+            if (err) {return res.serverError(err);}
+            res.view('patologia-mostrar', {patologias: rawResult }); 
+
+        }); 
+    }
+
+
+
+      
 };
 
