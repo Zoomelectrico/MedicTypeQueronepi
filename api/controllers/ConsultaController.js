56156d9e6249a4fa58fed0e691d95fc61600a1c0
ClauNL <32688500+ClauNL@users.noreply.github.com>
2017-11-15 00:05:23 -0400

    miles de cambios en todo

--- api/controllers/ConsultaController.js
+++ api/controllers/ConsultaController.js
@@ -6,76 +6,87 @@
  */
 
 module.exports = {
-	Crear: function(req, res, next, callback) {
-	    
-	    var params = req.body;
-	    Consulta.create(params, function(err, createdData) {
-	      if (err) {
-	        return res.badRequest({error: err});
-	      } else {
-	        res.view('medico-historia-medica', {medico: med, consulta:createdData}); 
-	      }
-	    });
-    },
+	Crear: function (req, res, next, callback) {
+		console.log(req.body);
+		console.log(req.params.idMedico);
+		console.log(req.params.idPaciente);
+		var hoy = new Date();
+		var dateAnio = hoy.getFullYear();
+		var dateMes = hoy.getMonth() + 1;
+		var dateDia = hoy.getDate();
+		var fecha = '\'' + dateAnio + '-' + dateMes + '-' + dateDia + '\'';
 
-    Agendar: function(req, res){
-    	console.log(req.params);
-	    var med;
-	    Medico.findOne({id:req.params.idMedico}).exec(function(err, medico){
-	    	if(err){
-	    		return res.badRequest({error: err});
-	    	}else{
-	    		console.log(medico);
-	    		med = medico;
-	    		res.view('medico-agenda', {medico:med});
-	    	};
-	    });
-    },
-
-	Buscar: function(req, res, next, callback) {
-	    var consulta = req.body;
-	    Consutla.findOne(consulta, function(err, createdData){
-	      if (err) {
-	        return res.badRequest({error: err});
-	      } else {
-	        res.view('medico-historia-medica', {Consulta:createdData});
-	      }
-	    });
- 	},
-
- 	Modificar: function (req, res) {
-    Consulta.findOne({ Cedula: req.body.cedula }).exec(function (err, consulta) {
-      if (err) {
-        return res.badRequest({ error: err });
-      }
-      console.log(consulta);
-      res.view('medico-consulta', { Consulta: consulta });
-    });
+		Consulta.create({ Peso: req.body.Peso, Talla: req.body.Talla, Tension: req.body.Tension, 
+			Temperatura: req.body.Temperatura, FRespiratoria: req.body.FRespiratoria, 
+			FCardiaca: req.body.FCardiaca, Medico: req.params.idMedico, 
+			Paciente: req.params.idPaciente, Fecha: fecha }, function (err, createdData) {
+			if (err) {
+				return res.badRequest({ error: err });
+			} else {
+				console.log(createdData);
+			    res.redirect('/Medico/BuscarPorID/'+req.params.idMedico)
+			}
+		});
 	},
-	
+
+	Agendar: function (req, res) {
+		console.log(req.params);
+		var med;
+		Medico.findOne({ id: req.params.idMedico }).exec(function (err, medico) {
+			if (err) {
+				return res.badRequest({ error: err });
+			} else {
+				console.log(medico);
+				med = medico;
+				res.view('medico-agenda', { medico: med });
+			};
+		});
+	},
+
+	Buscar: function (req, res, next, callback) {
+		var consulta = req.body;
+		Consutla.findOne(consulta, function (err, createdData) {
+			if (err) {
+				return res.badRequest({ error: err });
+			} else {
+				res.view('medico-historia-medica', { Consulta: createdData });
+			}
+		});
+	},
+
+	Modificar: function (req, res) {
+		Consulta.findOne({ Cedula: req.body.cedula }).exec(function (err, consulta) {
+			if (err) {
+				return res.badRequest({ error: err });
+			}
+			console.log(consulta);
+			res.view('medico-consulta', { Consulta: consulta });
+		});
+	},
+
 	BuscarModificar: function (req, res) {
 		console.log(req.params);
 		var doc;
 		var paciente;
-		Medico.findOne({id:req.params.idMedico}).exec(function (err, medico){
+		Medico.findOne({ id: req.params.idMedico }).exec(function (err, medico) {
 			if (err) {
-        		return res.badRequest({ error: err });
-      	} else{
+				return res.badRequest({ error: err });
+			} else {
 				console.log(medico);
 				doc = medico;
 			};
 		});
-		Paciente.findOne({id:req.params.idPaciente}).exec(function (err, paci){
+		Paciente.findOne({ id: req.params.idPaciente }).exec(function (err, paci) {
 			if (err) {
-        return res.badRequest({ error: err });
-      } else{
+				return res.badRequest({ error: err });
+			} else {
 				console.log(paci);
 				paciente = paci;
-				res.view('medico-consulta', {medico: doc, paciente: paciente});
+				res.view('medico-consulta', { medico: doc, paciente: paciente });
 			};
 		});
-		
-	
+
+
 	}
 
 };
